ed805561544b62ca4a98b6d1a9b9519567e566be
GabiR1221 <gabrielrusu0411@gmail.com>
2026-04-03 15:47:11 +0300

    PetSaveManager.lua

--- PetSaveManager.lua
+++ PetSaveManager.lua
@@ -5,7 +5,7 @@ local Players = game:GetService("Players")
 function PetSaveManager:Initialize(dataStoreName, stateTable, carryingTable)
 	self.petState = stateTable or {}
 	self.carryingPetByUserId = carryingTable or {}
-	self.dataStore = DataStoreService:GetDataStore("PetData1")
+	self.dataStore = DataStoreService:GetDataStore("PetData15")--------------------------CHANGE THIS TO CHANGE PETDATA
 	self.saveQueue = {} -- userId -> data to save
 	return self  -- ← ADD THIS LINE
 end
