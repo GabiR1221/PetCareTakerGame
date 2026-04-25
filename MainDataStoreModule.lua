81e54c071f63b3b5cdb5cbde7174554362060113
GabiR1221 <gabrielrusu0411@gmail.com>
2026-04-25 12:24:57 +0300

    Create MainDataStoreModule.lua

---
+++ MainDataStoreModule.lua
@@ -0,0 +1,19 @@
+--DataStoreModule in ServerScriptService-DataStore(folder)
+local LoadingFunctions = require(script.Loading)
+local SavingFunctions = require(script.Saving)
+
+local Datastore = {
+	LoadData = function(Player)
+		LoadingFunctions.LoadData(Player)
+		
+		return true
+	end,
+	
+	SaveData = function(Player, AutoSave)
+		SavingFunctions.SaveData(Player, AutoSave)
+		
+		return true
+	end,
+}
+
+return Datastore
